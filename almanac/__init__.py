188eb75c31321ad960f3819239f3595cd19fc7bb
Matt George <mgeorge@stratasan.com>
2012-11-17 13:13:56 -0600

    cleaning up imports for models

--- almanac/__init__.py
+++ almanac/__init__.py
@@ -1,11 +1,6 @@
-import os
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
-from .models import initialize_sql
-from .models import (
-    DBSession,
-    Base,
-)
+from almanac.models import initialize_sql
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -15,8 +10,6 @@ def main(global_config, **settings):
     #    settings['sqlalchemy.url'] = database_url
     engine = engine_from_config(settings, 'sqlalchemy.')
     initialize_sql(engine)
-    #DBSession.configure(bind=engine)
-    #Base.metadata.bind = engine
     config = Configurator(settings=settings)
     config.add_tween('pyramid_spine.tweens.timezone_tween_factory')
     config.include('pyramid_jinja2')
