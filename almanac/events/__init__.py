6647303f8ee58baa557a06134caca84568e075dd
Matt George <mgeorge@stratasan.com>
2013-01-10 20:52:42 -0600

    adding better event listing

--- almanac/events/__init__.py
+++ almanac/events/__init__.py
@@ -1,5 +1,6 @@
 def includeme(config):
     config.add_route('event.list', '')
+    config.add_route('events.ics', 'events.ics')
     config.add_route('event.new', '/new')
     config.add_route('event.detail', '/{id}')
     config.scan()
--- almanac/events/views.py
+++ almanac/events/views.py
@@ -2,11 +2,13 @@ from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound, HTTPNotFound
 
 from almanac.models import Event
+from sqlalchemy import func
+from sqlalchemy import Date, cast
 
 @view_config(route_name='event.list', renderer="events/list.html")
 @view_config(route_name='events.ics')
 def list(request):
-    events = Event.query.order_by(Event.start_time.desc())
+    events = Event.query.filter(cast(Event.start_time, Date) >= func.current_date()).order_by(Event.start_time.asc())
     return {'events': events}
 
 @view_config(route_name='event.detail', renderer="events/detail.html")
@@ -16,3 +18,7 @@ def detail(request):
     if not event:
         return HTTPNotFound('')
     return {'event': event}
+
+@view_config(route_name='event.new', renderer='events/new.html')
+def new(request):
+    return {}
